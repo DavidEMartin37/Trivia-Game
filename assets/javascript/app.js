2a2a0978d7d156827f8dda57ccf69c76cc1c90fe
David Martin <demartins37@yahoo.com>
2019-07-09 18:54:46 -0400

    changes and images added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,24 @@
 var question1 = {
   question: "The series 'Stranger Things' takes place in the town of Hawkins, IN: ",
-  answer: "true"
+  answer: "True"
 };  
 var question2 = {
-  question: "In the series 'Stranger Things' the telekenetic girl is called Eleven: ",
-  answer: "true"
+  question: "In the series 'Stranger Things' the telekinetic girl is called Eleven: ",
+  answer: "True"
 };  
 var question3 = {
   question: "In the series 'Stranger Things' the four main boys play Dungeons and Dragons: ",
-  answer: "true"
+  answer: "True"
 };  
-  
+var question4 = {
+  question: "In the series 'Stranger Things' the local police chief is called Hutch: ",
+  answer: "False"
+};  
+
 var timerId;
 var correct = 0;
 var incorrect = 0;
-var counter = 60;
+var counter = 30;
 
 function clock() {
   counter--;
@@ -25,20 +29,24 @@ function clock() {
 };
 
 function startGame() {
-  $("#question1").html(question1.question);
-  $("#question2").append(question2.question);
+  $("#question1").text(question1.question);
+  $("#question2").text(question2.question);
   $("#question3").text(question3.question);
+  $("#question4").text(question4.question);
   timerId = setInterval(clock, 1000);
 };
 
 function checkResult() {
-  if (question1.answer === "true") {
+  if (question1.answer === $("#answer1").val()) {
     correct++;
   }else incorrect++;
-  if (question2.answer === "true") {
+  if (question2.answer === $("#answer2").val()) {
     correct++;
   }else incorrect++;
-  if (question3.answer === "true") {
+  if (question3.answer === $("#answer3").val()) {
+    correct++;
+  }else incorrect++;
+  if (question4.answer === $("#answer4").val()) {
     correct++;
   }else incorrect++;
 };
@@ -47,14 +55,13 @@ function gameOver() {
   clearInterval(timerId);
   checkResult();
   $(".display-timer").text("Game Over");
-  counter = 60;
-  $("#answer1").html("<div>Correct: " + correct + "Incorrect: " + incorrect + "</div>");
-  $("#answer2").html("<div>Correct: " + correct + "Incorrect: " + incorrect + "</div>");
-  $("#answer3").html("<div>Correct: " + correct + "Incorrect: " + incorrect + "</div>");
+  counter = 30;
+  $("#results").html("<div>Correct: " + correct + " Incorrect: " + incorrect + "</div>");
 };
 
 $(".start").on("click", startGame); 
-  
+$(".submit").on("click", gameOver); 
+
 
 
 
